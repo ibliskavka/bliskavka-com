e6346f45b154aea37e4af26d7d968bbbb2445377
Ivan Bliskavka <ibliskavka@live.com>
2018-05-11 14:39:54 -0400

    tags

--- deploy-stack.cmd
+++ deploy-stack.cmd
@@ -1,1 +1,1 @@
-aws cloudformation deploy --template-file ./Aws/CloudFormation/Hosting/stack-template.yml --stack-name bliskavka-com-stack --tags Client=Bliskavka Project=Home
+aws cloudformation deploy --template-file ./Aws/CloudFormation/Hosting/stack-template.yml --stack-name bliskavka-com-stack --tags Client=Bliskavka Project=Home Environment=Production
