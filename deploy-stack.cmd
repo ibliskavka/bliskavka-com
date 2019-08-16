f4f7fa856726b3a8dc425fb2d26cd1f0019b8971
Ivan Bliskavka <ibliskavka@live.com>
2019-08-16 14:13:42 -0400

    increase cache time

--- deploy-stack.cmd
+++ deploy-stack.cmd
@@ -1,1 +1,7 @@
-aws cloudformation deploy --template-file ./Aws/CloudFormation/Hosting/stack-template.yml --stack-name bliskavka-com-stack --tags Client=Bliskavka Project=Home Environment=Production
+aws cloudformation deploy^
+    --template-file ./Aws/CloudFormation/Hosting/stack-template.yml^
+    --stack-name bliskavka-com-stack^
+    --tags^
+        Client=Bliskavka^
+        Project=Home^
+        Environment=Production
